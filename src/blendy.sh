8dd5c7bdf8c7d392cf86c122a21ead4126c95b45
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2021-07-29 15:32:03 +0900

    引数の順番を変えた。

--- src/blendy.sh
+++ src/blendy.sh
@@ -7,8 +7,8 @@
 convert $1 -resize '240x240!' left.png
 convert $2 -resize '240x240!' right.png
 rm -f *.gif
-blendy left.png right.png
+racket blendy.rkt left.png right.png
 rm left.png right.png
-png-to-gif.sh
+sh png-to-gif.sh
 gifsicle --colors 256 -l3 *.gif -o $3
-firefox $3
+#firefox $3
